58daa3e5a9caa43a432f5c017e549be575890b25
Lewis Knorr <lewisknorr@gmail.com>
2018-03-31 11:35:51 -0600

    adding code and stuff

---
+++ evestatus/evestatus.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+import pyping
+
+class evestatus:
+    """Check the server status of Tranquility"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def tq(self):
+        """This does stuff!"""
+
+        #Your code will go here
+        r = pyping.ping('https://esi.tech.ccp.is/latest/status/?datasource=tranquility')
+
+        if r.ret_code == 0:
+            print("Tranquility is online")
+        else:
+            print("Tranquility is offline")
+
+
+def setup(bot):
+    bot.add_cog(tq(bot))
