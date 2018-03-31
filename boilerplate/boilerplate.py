4839bf1a6ecc3d373d773796a0f7d120f7b9f7fa
Lewis Knorr <lewisknorr@gmail.com>
2018-03-31 11:45:39 -0600

    Proper class naming for help screen

--- boilerplate/boilerplate.py
+++ boilerplate/boilerplate.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-class Mycog:
+class FedoCogs:
     """My custom cog that does stuff!"""
 
     def __init__(self, bot):
@@ -15,4 +15,4 @@ class Mycog:
         await self.bot.say("I can do stuff!")
 
 def setup(bot):
-    bot.add_cog(Mycog(bot))
+    bot.add_cog(FedoCogs(bot))
