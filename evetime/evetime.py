f49f525c8a1ccb0e955617d7ce9ce9301bd6385e
Lewis Knorr <lewisknorr@gmail.com>
2018-04-19 09:32:56 -0600

    added evetime cog files

---
+++ evetime/evetime.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+from time import gmtime,strftime
+
+class EVETime:
+    """A simple cog to output the current EVE time"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def evetime(self):
+        """A simple cog to output the current EVE time"""
+
+        #Your code will go here
+        await self.bot.say(strftime("Current EVE Time: %m/%d/%Y - %H:%M:%S", gmtime()))
+
+def setup(bot):
+    bot.add_cog(EVETime(bot))
