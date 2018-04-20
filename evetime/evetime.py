23a44f194d80f99c045e18afbe20b78195558384
Lewis Knorr <lewisknorr@gmail.com>
2018-04-20 11:36:15 -0600

    Removed unnecessary lines

--- evetime/evetime.py
+++ evetime/evetime.py
@@ -3,14 +3,13 @@ from discord.ext import commands
 from time import gmtime,strftime
 
 class EVETime:
-    """A simple cog to output the current EVE time"""
+    """A simple cog to output the current time in EVE Online"""
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
     async def evetime(self):
-        """A simple cog to output the current EVE time"""
 
         #Your code will go here
         await self.bot.say(strftime("EVE time is currently **%H:%M:%S** on **%d/%m/%Y**", gmtime()))
